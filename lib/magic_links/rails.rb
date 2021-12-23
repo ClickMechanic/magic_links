e06ce2b171cbdc2549ef67e53315353fcf7b44ae
wozza35 <wozza35@hotmail.com>
2021-12-23 21:52:30 +0000

    Rename initializers
    To be more in line with convention

--- lib/magic_links/rails.rb
+++ lib/magic_links/rails.rb
@@ -1,13 +1,13 @@
 module MagicLinks
   class Engine < ::Rails::Engine
 
-    initializer 'magic links url helpers' do
+    initializer 'magic_links.url_helpers' do
       ActiveSupport.on_load(:action_controller) do
         include MagicLinks::MagicLinksHelper
       end
     end
 
-    initializer 'magic links middleware', before: :build_middleware_stack do |app|
+    initializer 'magic_links.middleware_redirect', before: :build_middleware_stack do |app|
       app.config.middleware.insert_after ActionDispatch::Cookies, MagicLinks::MagicTokenRedirect
     end
 
