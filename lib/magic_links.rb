06a4ca094f51a3e08b1b2551ec42d5e5d453c9b7
James Wozniak <wozza35@hotmail.com>
2022-02-06 23:22:31 +0000

    Autoload MagicTokenAuthentication strategy instead of require
    Not much in it, but autoload will mean the file is not loaded until it is used.

--- lib/magic_links.rb
+++ lib/magic_links.rb
@@ -1,7 +1,8 @@
 require "magic_links/engine"
 require 'magic_links/rails'
-require 'magic_links/strategies/magic_token_authentication'
 
 module MagicLinks
-  # Your code goes here...
+  module Strategies
+    autoload :MagicTokenAuthentication, 'magic_links/strategies/magic_token_authentication'
+  end
 end
