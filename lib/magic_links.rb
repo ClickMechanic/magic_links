d4306beb26e1d2b1be9fe90dbeedee18688e28be
James Wozniak <wozza35@hotmail.com>
2022-05-29 16:05:05 +0100

    Do not autoload classes
    The autoloading was kind of pointless, as the gem initialization would require these files be loaded anyway.

--- lib/magic_links.rb
+++ lib/magic_links.rb
@@ -1,17 +1,11 @@
 require 'magic_links/engine'
 require 'magic_links/template'
 require 'magic_links/templates'
+require 'magic_links/middleware/magic_token_redirect'
+require 'magic_links/strategies/magic_token_authentication'
 require 'magic_links/rails'
 
 module MagicLinks
-  module Strategies
-    autoload :MagicTokenAuthentication, 'magic_links/strategies/magic_token_authentication'
-  end
-
-  module Middleware
-    autoload :MagicTokenRedirect, 'magic_links/middleware/magic_token_redirect'
-  end
-
   def self.add_template(*args)
     MagicLinks::Templates.add(*args)
   end
